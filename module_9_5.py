bf1c0b8574c23093ee6a43862d3700b16559db23
nApTu3aH82 <rinsy89@gmail.com>
2024-09-24 14:40:49 +0500

    Add files via upload

---
+++ module_9_5.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+class StepValueError(ValueError):
+    def __init__(self):
+        pass
+
+
+class Iterator:
+    def __init__(self, start, stop, step=1):
+        self.start = start
+        self.stop = stop
+        self.step = step
+        Iterator.__is_valid_step(self)
+
+    def __is_valid_step(self):
+        if self.step == 0:
+            raise StepValueError
+        return True
+
+    def __iter__(self):
+        self.pointer = self.start - self.step
+        return self
+
+    def __next__(self):
+        self.pointer += self.step
+        if self.step < 0 and self.pointer < self.stop:
+            raise StopIteration()
+        if self.step > 0 and self.pointer > self.stop:
+            raise StopIteration()
+        return self.pointer
+
+
+try:
+    iter1 = Iterator(100, 200, 0)
+    for i in iter1:
+        print(i, end=' ')
+    print()
+except StepValueError:
+    print('Шаг указан неверно')
+try:
+    iter2 = Iterator(-5, 1)
+    for i in iter2:
+        print(i, end=' ')
+    print()
+except StepValueError:
+    print('Шаг указан неверно')
+try:
+    iter3 = Iterator(6, 15, 2)
+    for i in iter3:
+        print(i, end=' ')
+    print()
+except StepValueError:
+    print('Шаг указан неверно')
+try:
+    iter4 = Iterator(5, 1, -1)
+    for i in iter4:
+        print(i, end=' ')
+    print()
+except StepValueError:
+    print('Шаг указан неверно')
+try:
+    iter5 = Iterator(10, 1)
+    for i in iter5:
+        print(i, end=' ')
+    print()
+except StepValueError:
+    print('Шаг указан неверно')
